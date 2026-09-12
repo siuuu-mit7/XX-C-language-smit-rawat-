278e422c37b33f05cb16e4775529f5592fbb4dbc
siuuu-mit7 <smitrawat49@gmail.com>
2026-09-13 00:00:01 +0530

    Add files via upload

---
+++ lab/04/lab4_logical_operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int element1=1;
+int element2=0;
+
+printf("AND operator: %d\n", element1&&element2);
+printf("OR operator: %d\n", element1||element2);
+printf("NOT operator for element1: %d\n",!element1 );
+printf("NOT operator for element2: %d\n",!element2);
+ return 0;
+
+
+
+
+
+}
