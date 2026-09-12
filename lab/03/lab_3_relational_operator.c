9021165049d6a4b66c11845a826b5ca509010e93
siuuu-mit7 <smitrawat49@gmail.com>
2026-09-12 23:56:52 +0530

    Add files via upload

---
+++ lab/03/lab_3_relational_operator.c
@@ -0,0 +1,14 @@
+// relational operator
+#include<stdio.h>
+int main(){
+int num1=3;
+int num2=5;
+printf("num1 is smaller than  num2 = %d\n",num1<num2);
+printf("num1 is greaater than num2 = %d\n",num1>num2);
+printf("num1 is less than equal to num2 = %d\n",num1<=num2);
+printf("num1 greater than equal to num2 = %d\n",num1>=num2);
+printf("num1 is Not equal to num2 = %d\n",num1!=num2);
+printf("num1 is equal to num2 = %d\n",num1==num2);
+return 0;
+
+}
