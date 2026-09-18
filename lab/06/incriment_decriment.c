480561cab587ea2b78997d69a8e616108345254a
siuuu-mit7 <smitrawat49@gmail.com>
2026-09-19 01:16:49 +0530

    Add files via upload

---
+++ lab/06/incriment_decriment.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int num ;
+printf("enter your number :");
+ scanf("%d",&num);
+ printf("value of number :%d\n",num);
+++num;
+
+ printf("value after incrimnet :%d\n",num);
+ 
+--num;
+printf("value after decrimnt :%d\n",num);
+
+
+
+return 0;
+
+}
