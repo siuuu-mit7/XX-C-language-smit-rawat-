1e5eca2dc54a7e7f6929539913701edb453e57a2
siuuu-mit7 <smitrawat49@gmail.com>
2026-09-19 00:24:37 +0530

    Add files via upload

---
+++ lab/05/lab3_bitwise_operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+int a=15 , b=5;
+printf("a&b=%d\n",a&b);
+printf("a|b=%d\n",a|b);
+printf("a^b=%d\n",a^b);
+printf("~b=%d\n",~b);
+printf("a>>b=%d\n",a>>b);
+printf("a<<b=%d\n",a<<b);
+
+
+return 0;
+
+
+
+}
